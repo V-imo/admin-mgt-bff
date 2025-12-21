7fb99491583b2727e2813572b73928950a59e019
Alfred Gauthier <alfgoto@gmail.com>
2025-12-21 22:23:07 +0100

    feat: update vimo

--- src/functions/listener.ts
+++ src/functions/listener.ts
@@ -14,9 +14,7 @@ export const handler = async (
   event: EventBridgeEvent<string, EventEnvelope>
 ) => {
   if (event["detail-type"] === AgencyUpdatedEvent.type) {
-    console.log("AgencyUpdatedEvent", event.detail);
     const detail = AgencyUpdatedEvent.parse(event.detail);
-    console.log("detail", detail);
     await Agency.update({
       ...detail.data,
       oplock: detail.timestamp,
